e7b302b43d219b5ac137f1610adf2e0c9fb8917f
NoBey <786964300@qq.com>
2016-08-29 01:23:10 +0800

    s

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 var smtp = require('smtp-protocol');
 var mailparser = require("mailparser").MailParser;
 var server = smtp.createServer({
-  domain:'nobey.cn'
-},function (req) {
-  req.on('greeting', function (to, ack) {
-      console.log(to)
-      ack.accept();
-});
-req.on('from', function (to, ack) {
+  domain: 'nobey.cn'
+}, function(req) {
+
+  req.on('greeting', function(to, ack) {
+    ack.accept();
+  });
+
+  req.on('from', function(to, ack) {
     console.log(to)
-ack.accept();
-});
-    req.on('to', function (to, ack) {
-      console.log(to)
-        var domain = to.split('@')[1] || 'localhost';
-        if (domain === 'localhost') ack.accept()
-        else ack.reject()
+    ack.accept();
+  });
 
+  req.on('to', function(to, ack) {
+    console.log(to)
+    var domain = to.split('@')[1] || 'localhost';
+    if (domain === 'localhost') ack.accept()
+    else ack.reject()
+  });
+
+  req.on('message', function(stream, ack) {
+    console.log('from: ' + req.from);
+    console.log('to: ' + req.to);
+    stream.pipe(process.stdout, {
+      end: false
     });
-    req.on('message', function (stream, ack) {
-        console.log('from: ' + req.from);
-        console.log('to: ' + req.to);
-
-        stream.pipe(process.stdout, { end : false });
-        ack.accept();
-    });
+    ack.accept();
+  });
 
 });
 
-server.listen(9025);
-console.log('9025')
+server.listen(25);
+console.log('25')
