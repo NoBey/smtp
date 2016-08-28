fc281a67ba8c5002a7106a3e176a1e6de13b0073
NoBey <786964300@qq.com>
2016-08-29 02:27:53 +0800

    s

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 smtp.connect('mx1.qq.com', 25, function (mail) {
     mail.helo('nobey.cn');
     mail.from('nobey@nobey.cn');
-    mail.to('786964300@qq.cn');
+    mail.to('786964300@qq.com');
     mail.data();
     fs.createReadStream('foo.txt').pipe(mail.message());
     // console.log(mail);
