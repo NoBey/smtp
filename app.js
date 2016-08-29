fb7cf23552c1ea2428e17aa563e0c9b102423b00
NoBey <786964300@qq.com>
2016-08-30 00:46:16 +0800

    ok

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var smtp = require('smtp-protocol');
+
 var MailParser = require("mailparser").MailParser;
 var mailparser = new MailParser();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('smtp://mx1.qq.com');
 
 var Domain = 'nobey.cn'
-
 var server = smtp.createServer({
   domain: 'nobey.cn'
 }, function(req) {
-
   req.on('greeting', function(to, ack) {
     ack.accept();
   });
@@ -24,14 +25,40 @@ var server = smtp.createServer({
 
   req.on('message', function(stream, ack) {
 
- stream.pipe(mailparser);
- mailparser.on("end", function(mail_object){
-  console.log(mail_object); //这里就是解析好的mail格式
- })
+function sendQQ(){
+  var mailOptions = {
+      from: 'nobey@nobey.cn',
+      to: '786964300@qq.com',
+      subject: 'Hello',
+      text: 'Hello world',
+      html: '<b>Hello world</b>'
+  };
+  stream.pipe(mailparser);
+  mailparser.on("end", function(mail_object){
+    mailOptions = mail_object;
+    mailOptions.html = mail_object.from.address + '<br/>' +  mail_object.html
+    mailOptions.to = '786964300@qq.com';
+    mailOptions.from = 'nobey@nobey.cn';
+    delete mailOptions.headers;
+    delete mailOptions.messageId;
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error) return console.log(error);
+      console.log('Message sent: ' + info.response);
+  })
+  })
+}
+
+    req.from.split('@')[1] === 'qq.com' ? sendQQ() : smtp.connect('mx1.qq.com', 25, function (mail) {
+        mail.helo('mx1.qq.com');
+        mail.from('nobey@nobey.cn');
+        mail.to('786964300@qq.com');
+        mail.data();
+        stream.pipe(mail.message());
+        mail.quit();
+    });
+
     ack.accept();
   });
 
 });
-
 server.listen(25);
-console.log('25')
---
+++ app00.js
@@ -0,0 +1,37 @@
+var smtp = require('smtp-protocol');
+var MailParser = require("mailparser").MailParser;
+var mailparser = new MailParser();
+
+var Domain = 'nobey.cn'
+
+var server = smtp.createServer({
+  domain: 'nobey.cn'
+}, function(req) {
+
+  req.on('greeting', function(to, ack) {
+    ack.accept();
+  });
+
+  req.on('from', function(to, ack) {
+    ack.accept();
+  });
+
+  req.on('to', function(to, ack) {
+    var domain = to.split('@')[1] || Domain;
+    if (domain === Domain)  ack.accept()
+    else ack.reject()
+  });
+
+  req.on('message', function(stream, ack) {
+
+ stream.pipe(mailparser);
+ mailparser.on("end", function(mail_object){
+  console.log(mail_object); //这里就是解析好的mail格式
+ })
+    ack.accept();
+  });
+
+});
+
+server.listen(25);
+console.log('25')
