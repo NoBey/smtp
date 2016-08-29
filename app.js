7aa01fe55d4be76da560da4c96b9460512f4d4f5
NoBey <786964300@qq.com>
2016-08-30 00:13:39 +0800

    s

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ var server = smtp.createServer({
 
   req.on('message', function(stream, ack) {
 
-stream.pipe(mailparser);
-
+ stream.pipe(mailparser);
  mailparser.on("end", function(mail_object){
   console.log(mail_object); //这里就是解析好的mail格式
  })
--- app0.js
+++ app0.js
@@ -1,9 +1,14 @@
 var smtp = require('smtp-protocol');
+
+var MailParser = require("mailparser").MailParser;
+var mailparser = new MailParser();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport('smtp://mx1.qq.com');
+
 var Domain = 'nobey.cn'
 var server = smtp.createServer({
   domain: 'nobey.cn'
 }, function(req) {
-
   req.on('greeting', function(to, ack) {
     ack.accept();
   });
@@ -19,17 +24,31 @@ var server = smtp.createServer({
   });
 
   req.on('message', function(stream, ack) {
-    console.log('message')
-    smtp.connect('mx1.qq.com', 25, function (mail) {
+
+    var mailOptions = {
+        from: 'nobey@nobey.cn',
+        to: '786964300@qq.com',
+        subject: 'Hello',
+        text: 'Hello world',
+        html: '<b>Hello world</b>'
+    };
+    stream.pipe(mailparser);
+    mailparser.on("end", function(mail_object){
+      mailOptions = mail_object
+      mailOptions.to = '786964300@qq.com'
+    })
+
+    req.from.split('@')[1] === 'qq.com' ? transporter.sendMail(mailOptions, function(error, info){
+      if(error) return console.log(error);
+      console.log('Message sent: ' + info.response);
+  }) : smtp.connect('mx1.qq.com', 25, function (mail) {
         mail.helo('mx1.qq.com');
         mail.from('nobey@nobey.cn');
         mail.to('786964300@qq.com');
         mail.data();
         stream.pipe(mail.message());
         mail.quit();
-        console.log('ok')
     });
-
 
     ack.accept();
   });
@@ -37,4 +56,4 @@ var server = smtp.createServer({
 });
 
 server.listen(25);
-console.log('25')
+// console.log('25')
